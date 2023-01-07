637748eadcd6ff96990777e2c7203e5686d16ca6
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2023-01-08 01:13:25 +0200

    Add tests for DataTemplate and ElementTemplate

---
+++ src/BlazorBindings.UnitTests/Extensions/ControlsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace BlazorBindings.UnitTests.Extensions
+{
+    public static class ControlsExtensions
+    {
+        public static T GetTemplateContent<T>(this TemplatedView view)
+        {
+            return (T)((Layout)view.Children[0]).Children[0];
+        }
+
+        public static T GetTemplateContent<T>(this object templateRoot)
+        {
+            return (T)((Layout)templateRoot).Children[0];
+        }
+    }
+}
