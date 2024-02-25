ae065cd67e2da552336cad18307dead9b672f1b9
warappa <spoiltnr1@gmx.at>
2024-02-25 16:17:27 +0100

    Cleanup

---
+++ src/BlazorBindings.UnitTests/TestApplication.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Headless;
+using BlazorBindings.AvaloniaBindings;
+
+[assembly: AvaloniaTestApplication(typeof(BlazorBindings.UnitTests.TestApplication))]
+
+namespace BlazorBindings.UnitTests;
+
+public class TestApplication : BlazorBindingsApplication, IAvaloniaBlazorApplication, ITestApplication
+{
+    public Window Window { get; set; }
+
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<TestApplication>()
+        .UseSkia()
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions
+        {
+            UseHeadlessDrawing = false
+        })
+        .UseAvaloniaBlazorBindings(services =>
+        {
+            services.AddSingleton<AvaloniaBlazorBindingsRenderer, TestBlazorBindingsRenderer>();
+        });
+
+    public static TestApplication Create()
+    {
+        var builder = TestApplication.BuildAvaloniaApp();
+        builder.SetupWithoutStarting();
+        var application = (TestApplication)builder.Instance;
+
+        return application;
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        // Don't call base.OnFrameworkInitializationCompleted() because there it tries to render the main page
+    }
+}
--- src/BlazorBindings.UnitTests/TestBlazorBindingsRenderer.cs
+++ src/BlazorBindings.UnitTests/TestBlazorBindingsRenderer.cs
@@ -1,51 +1,8 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Headless;
-using BlazorBindings.AvaloniaBindings;
-using BlazorBindings.AvaloniaBindings.Navigation;
-using BlazorBindings.Core;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using BlazorBindings.AvaloniaBindings;
 using Microsoft.Extensions.Logging;
-using System.Reflection;
 using System.Runtime.ExceptionServices;
-using System.Threading;
-//using MauiDispatching = Microsoft.Maui.Dispatching;
-
-[assembly: AvaloniaTestApplication(typeof(BlazorBindings.UnitTests.TestApplication))]
 
 namespace BlazorBindings.UnitTests;
-
-
-public class TestApplication : BlazorBindingsApplication, IAvaloniaBlazorApplication, ITestApplication
-{
-    public Window Window { get; set; }
-
-    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<TestApplication>()
-        .UseSkia()
-        .UseHeadless(new AvaloniaHeadlessPlatformOptions
-        {
-            UseHeadlessDrawing = false
-        })
-        .UseAvaloniaBlazorBindings(services =>
-        {
-            services.AddSingleton<AvaloniaBlazorBindingsRenderer, TestBlazorBindingsRenderer>();
-        });
-
-    public static TestApplication Create()
-    {
-        var builder = TestApplication.BuildAvaloniaApp();
-        builder.SetupWithoutStarting();
-        var application = (TestApplication)builder.Instance;
-
-        return application;
-    }
-
-    public override void OnFrameworkInitializationCompleted()
-    {
-        // Don't call base.OnFrameworkInitializationCompleted() because there it tries to render the main page
-    }
-}
 
 internal class TestBlazorBindingsRenderer : AvaloniaBlazorBindingsRenderer
 {
