7732cff96f1ef4fe080d62cea7bf36c7314d414d
warappa <spoiltnr1@gmx.at>
2024-02-28 01:10:56 +0100

    (WIP) Control Gallery sample compiles but fails to start

---
+++ samples/ControlGallery/ImageHelper.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace ControlGallery;
+
+public static class ImageHelper
+{
+    public static Bitmap LoadFromResource(Uri resourceUri)
+    {
+        return new Bitmap(AssetLoader.Open(resourceUri));
+    }
+
+    public static Bitmap LoadFromResource(string resourceUri)
+    {
+        return LoadFromResource(new Uri(resourceUri));
+    }
+
+    public static async Task<Bitmap?> LoadFromWeb(Uri url)
+    {
+        using var httpClient = new HttpClient();
+        try
+        {
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsByteArrayAsync();
+            return new Bitmap(new MemoryStream(data));
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"An error occurred while downloading image '{url}' : {ex.Message}");
+            return null;
+        }
+    }
+}
